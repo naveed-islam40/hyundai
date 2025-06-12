1de55e042cb74b5b1f0f382a2f392abcf265c6aa
naveed-islam40 <naveedislam20ni@gmail.com>
2025-06-12 21:09:08 +0500

    Update email.js

--- server/email.js
+++ server/email.js
@@ -22,7 +22,6 @@ export async function sendEmail({ to, subject, text, html }) {
     text,
     html,
   };
-console.log("Sending to:", customerInfo?.email);
 
   try {
     const info = await transporter.sendMail(mailOptions);
