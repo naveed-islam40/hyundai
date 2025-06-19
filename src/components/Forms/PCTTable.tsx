809607f872495650b110d095a23e6c745993771a
naveed-islam40 <naveedislam20ni@gmail.com>
2025-06-19 13:21:49 +0500

    fix the table space

--- src/components/Forms/PCTTable.tsx
+++ src/components/Forms/PCTTable.tsx
@@ -19,16 +19,17 @@ const PCTTable = () => {
 
   return (
     <div className="w-full overflow-x-auto">
-      <table className="w-full border-collapse space-y-3">
-        {pct?.map((item: any, index: number) => (
-          <div key={index} className="space-y-3">
-            <h1 className="text-lg font-bold">{item?.category}</h1>
+      {pct?.map((item: any, index: number) => (
+        <div key={index} className="space-y-2 mb-6 overflow-x-auto">
+          <h1 className="text-lg font-bold">{item?.category}</h1>
+
+          <table className="table-auto border-collapse w-auto max-w-full text-sm">
             <thead>
               <tr className="bg-gray-100">
                 {tableHeaders.map((header, index) => (
                   <th
                     key={index}
-                    className="border border-gray-300 px-3 py-2 text-left text-xs font-medium text-gray-700 whitespace-nowrap"
+                    className="border border-gray-300 px-3 py-2 text-left font-medium text-gray-700 whitespace-nowrap"
                   >
                     {header}
                   </th>
@@ -37,47 +38,47 @@ const PCTTable = () => {
             </thead>
 
             <tbody>
-              {item?.items?.map((row: any, index: number) => (
-                <tr key={index} className="bg-white hover:bg-gray-50">
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.id}
+              {item?.items?.map((row: any, rowIndex: number) => (
+                <tr key={rowIndex} className="bg-white hover:bg-gray-50">
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
+                    {row.id}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.vehicle?.split(" ")[1]}
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
+                    {row.vehicle?.split(" ").slice(1).join(" ")}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900 w-[100px]">
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
                     {row.year}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.repair_blend}
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
+                    {row.repair_blend}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.category}
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap ">
+                    {row.category}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.operation}
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
+                    {row.operation}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.part}
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">
+                    {row.part}
                   </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row.body_labor ? `$ ${row.body_labor}` : "$0.00"}
-                  </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row.refinish_labor ? `$ ${row.refinish_labor}` : "$0.00"}
-                  </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.cost ? `$ ${row?.cost}` : "$0.00"}
-                  </td>
-                  <td className="border border-gray-300 px-3 py-2 text-sm text-gray-900">
-                    {row?.ccc_cost ? `$ ${row?.ccc_cost}` : "$0.00"}
-                  </td>
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">{`$ ${
+                    row.body_labor ?? "0.00"
+                  }`}</td>
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">{`$ ${
+                    row.refinish_labor ?? "0.00"
+                  }`}</td>
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">{`$ ${
+                    row.cost ?? "0.00"
+                  }`}</td>
+                  <td className="border border-gray-300 px-3 py-2 text-gray-900 whitespace-nowrap">{`$ ${
+                    row.ccc_cost ?? "0.00"
+                  }`}</td>
                 </tr>
               ))}
             </tbody>
-          </div>
-        ))}
-      </table>
+          </table>
+        </div>
+      ))}
     </div>
   );
 };
